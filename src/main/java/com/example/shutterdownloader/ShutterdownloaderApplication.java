0ebe65664e696f8d1a570bb86f04e2c9fb90d564
kamil <xkamilwrobel@gmail.com>
2020-02-05 19:03:36 +0100

    Initial commit

---
+++ src/main/java/com/example/shutterdownloader/ShutterdownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.shutterdownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutterdownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShutterdownloaderApplication.class, args);
+	}
+
+}
